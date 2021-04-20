168387eb546096532105366fe64859426710336c
ashiq4321 <ashiqulhoque45@gmail.com>
2021-04-21 00:37:13 +0600

    first component added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import reactDom from 'react-dom'
+
+import ReactDom from 'react-dom'
+function Greeting(){
+  return <h4>This the first component</h4>
+}
+
+reactDom.render(<Greeting/>, document.getElementById('root'));
