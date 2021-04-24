0977f7a66be626aa755dd09a90d2c0942c41eb46
ashiq4321 <ashiqulhoque45@gmail.com>
2021-04-24 14:24:47 +0600

    properties

--- src/index.js
+++ src/index.js
@@ -2,28 +2,39 @@ import React from 'react'
 import reactDom from 'react-dom'
 
 import './index.css'
+
+//setup object
+const firstBook={
+  img: 'https://m.media-amazon.com/images/I/51kcX5PpaZL._AC_UY327_FMwebp_QL65_.jpg',
+  title:'The Alchemist, 25th Anniversary: A Fable About Following Your Dream',
+  author: 'by Paulo Coelho'
+}
+
+const secondBook={
+  img: 'https://m.media-amazon.com/images/I/71yo6O-GhkL._AC_UL480_FMwebp_QL65_.jpg',
+  title:'The Testaments: A Novel Kindle Edition',
+  author: 'by Margaret Atwood'
+}
 function Booklist(){
   return <div>
     <h1>Booklist</h1>
      <section className='booklist'>
-      < Book/>
-      < Book/>
-      < Book/>
-      < Book/>
-      < Book/>
-      < Book/>
-      < Book/>
+      < Book img={firstBook.img}
+            title={firstBook.title}
+            author={firstBook.author}/>
+      < Book img={secondBook.img}
+            title={secondBook.title}
+            author={secondBook.author}/>
      </section>
     </div>
   
 }
-const author='by Paulo Coelho';
-const Book=() =>{
-  const title= 'The Alchemist, 25th Anniversary: A Fable About Following Your Dream';
+const Book=(props) =>{
+  console.log(props)
   return <div className='book'>
-    <img src='https://m.media-amazon.com/images/I/51kcX5PpaZL._AC_UY327_FMwebp_QL65_.jpg'></img> 
-    <h3>{title}</h3>
-    <h4>{author.toUpperCase()}</h4>
+    <img src={props.img}></img> 
+    <h3>{props.title}</h3>
+    <h4>{props.author}</h4>
     </div>
 }
 
