4be4d476d604eb5831f673bca7bb9b8e528d7180
ashiq4321 <ashiqulhoque45@gmail.com>
2021-04-24 13:02:08 +0600

    nested components

--- src/index.js
+++ src/index.js
@@ -2,13 +2,44 @@ import React from 'react'
 import reactDom from 'react-dom'
 
 
-/* function Greeting(){
+function Booklist(){
   return <div>
-      <h1>hello </h1>
-    </div> ;
+    <h1>Booklist</h1>
+     < Book/>
+     < Book/>
+     < Book/>
+     < Book/>
+     < Book/>
+     < Book/>
+     < Book/>
+     < Book/>
+     < Book/>
+     < Book/>
+     < Book/>
+     < Book/>
+     < Book/>
+     < Book/>
+    </div>
   
-} */
- const Greeting=()=>{
-   return React.createElement('div',{},React.createElement('h1','{}','hello'));
- }
-reactDom.render(<Greeting/>, document.getElementById('root'));
+}
+
+const Book=() =>{
+  return <div>
+    <Image/> 
+    <Title />
+    by <Author />
+    </div>
+}
+
+const Image=()=>{
+return <img src='https://m.media-amazon.com/images/I/51kcX5PpaZL._AC_UY327_FMwebp_QL65_.jpg'></img>
+}
+
+const Title=()=>{
+return <h3>The Alchemist, 25th Anniversary: A Fable About Following Your Dream</h3>
+}
+
+const Author=()=>{
+return <h4> Paulo Coelho</h4>
+}
+reactDom.render(<Booklist/>, document.getElementById('root'));
